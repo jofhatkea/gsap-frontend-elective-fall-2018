326a63824b72a5ebf0911c9e0d55f39558e5043e
Jonas Holbech <jofh@kea.dk>
2018-09-20 11:51:14 +0200

    final awesomeness

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const left = document.querySelectorAll("#lion #left path");
-TweenMax.staggerFrom(
-  left,
-  0.3,
-  {
-    x: -800,
-    rotation: 45
-  },
-  0.05
+const all = document.querySelectorAll("#lion #left path, #lion #right path");
+const right = document.querySelectorAll(
+  "#lion #right path, #lion #right  polygon"
 );
 
-const right = document.querySelectorAll("#lion #right path");
-TweenMax.staggerFrom(
-  right,
-  0.3,
+const tl = new TimelineMax({ repeat: -1, repeatDelay: 1 });
+//tl.timeScale(4);
+tl.staggerFrom(
+  all,
+  2.5,
   {
-    x: -800
+    x: () => Math.random() * 1000 - 500,
+    y: () => Math.random() * 1000 - 500,
+    ease: Elastic.easeIn.config(1, 0.3)
   },
-  0.05
+  0.02
 );
