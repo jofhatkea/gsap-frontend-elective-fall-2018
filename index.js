ae2c0d5bbcaf5247bd4e27bd24c8f7105c5106de
Jonas Holbech <jh@jonasholbech.dk>
2018-09-20 09:26:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+TweenMax.from("#app>div:nth-child(odd)", 3, { x: 500 });
+TweenMax.from("#app>div:nth-child(even)", 3, { x: -500 });
+TweenMax.from("#app>div:nth-child(3n+2)", 3, { y: 500 });
+TweenMax.from("#app>div:nth-child(3n+3)", 3, { y: -500 });
